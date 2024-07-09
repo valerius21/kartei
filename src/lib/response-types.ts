1fc866e8f3304ab09be81de37292724acabb3158
Valerius Mattfeld <mail@valerius.me>
2024-07-09 12:19:36 +0200

    basic word query

---
+++ src/lib/response-types.ts
@@ -0,0 +1,11 @@
+export interface Word {
+	collectionId: string;
+	collectionName: string;
+	created: Date;
+	english: string;
+	frequency: number;
+	id: string;
+	language: string;
+	native: string;
+	updated: Date;
+}
---
+++ src/routes/learn/[language]/+page.server.ts
@@ -0,0 +1,18 @@
+import { pb } from "$lib/pocketbase.js";
+import type { Word } from "$lib/response-types";
+import { error } from "@sveltejs/kit";
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ params }) {
+	const language = params.language;
+
+	const words = await pb.collection("words").getFullList<Word>({
+		filter: `language="${language}"`,
+	});
+
+	if (!words.length) {
+		error(404, "Not found");
+	}
+
+	return { words };
+}
